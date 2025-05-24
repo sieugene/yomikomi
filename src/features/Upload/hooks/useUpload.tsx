ab2e19606d1ed2959da249d538eb69a976a6c3b2
sieugene <sieugene@mail.ru>
2025-05-24 23:23:59 +0900

    feat: refactor import functionality, remove unused pages, and enhance upload handling

--- src/features/Upload/hooks/useUpload.tsx
+++ src/features/Upload/hooks/useUpload.tsx
@@ -1,12 +1,10 @@
 "use client";
 import { ApiClient } from "@/shared/api/api.client";
-import { useState } from "react";
 
 export const useUpload = () => {
-  const [file, setFile] = useState<File | null>(null);
-  const handleUpload = async () => {
+  const handleUpload = async (file: File | null) => {
     const response = await ApiClient.upload({ file, collectionName: "test" });
     console.log(response);
   };
-  return { setFile, handleUpload };
+  return { handleUpload };
 };
