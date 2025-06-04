326395d7bcc4ee1f9985322b4c4dc241e9347e57
sieugene <sieugene@mail.ru>
2025-06-04 22:31:12 +0900

    chore : fake db

--- src/features/AnkiParser/hooks/useAnkiParser.ts
+++ src/features/AnkiParser/hooks/useAnkiParser.ts
@@ -24,12 +24,13 @@ export const useAnkiParser = (type: string) => {
     const notes = Object.values(notesRaw).map((note) => {
       const model = models[note.mid] as { flds: { name: string }[] };
 
-      const fieldNames: string[] = model.flds.map((f) => f.name);
-      const values = note.flds.split("\x1f");
+      const fieldNames: string[] = model?.flds?.map((f) => f.name);
+      const values = note?.flds?.split?.("\x1f");
       const fields: Record<string, string> = {};
-      fieldNames.forEach((name, idx) => {
+      fieldNames?.forEach((name, idx) => {
         fields[name] = values[idx] ?? "";
       });
+
       return {
         id: note.id,
         fields,
