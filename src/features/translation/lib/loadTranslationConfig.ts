425d0fa20c883008ac40e403d2d000a2cb732035
sieugene <sieugene@mail.ru>
2026-02-11 23:44:42 +0900

    fix : ios load models

--- src/features/translation/lib/loadTranslationConfig.ts
+++ src/features/translation/lib/loadTranslationConfig.ts
@@ -3,7 +3,13 @@ import { TranslateConfig, TranslateSupportedLang } from "../types";
 import { SUPPORTED_TRANSLATIONS } from "./constants";
 import { env } from "@huggingface/transformers";
 
-env.allowLocalModels = true;
+
+if (env.backends.onnx.wasm) {
+  env.backends.onnx.wasm.numThreads = 1;
+  env.backends.onnx.wasm.simd = false;
+  env.backends.onnx.wasm.proxy = true;
+}
+
 
 export const loadTranslationConfig = async (
   language: TranslateSupportedLang,
@@ -11,9 +17,7 @@ export const loadTranslationConfig = async (
   const config = SUPPORTED_TRANSLATIONS[language];
   const modelsPromises = config.necessary_models.map(
     (modelName) => async () => {
-      const model = await pipeline("translation", modelName, {
-        local_files_only: true,
-      });
+      const model = await pipeline("translation", modelName);
       return { model, modelName };
     },
   );
