cc9dc88513a461927b7aeaff688b0d919c4e4f48
sieugene <eugenepoluakov@gmail.com>
2026-02-23 22:41:22 +0900

    feat : support en and jp metadata html locales

---
+++ src/app/[lang]/layout.tsx
@@ -0,0 +1,43 @@
+import { AppLayout } from "@/layouts/app.layout";
+import { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { Toaster } from "@/shared/ui/sonner";
+import { PAGE_METADA } from "@/shared/metadata/page.metadata";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export async function generateStaticParams() {
+  return [{ lang: "en" }, { lang: "ja" }];
+}
+
+export async function generateMetadata({
+  params,
+}: LayoutProps<"/[lang]">): Promise<Metadata> {
+  const { lang } = await params;
+  return PAGE_METADA[(lang as "en" | "jp")] ?? PAGE_METADA.en;
+}
+
+export default async function RootLayout({
+  children,
+  params,
+}: LayoutProps<"/[lang]">) {
+  return (
+    <html lang={(await params).lang}>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen bg-background text-foreground`}
+      >
+        <AppLayout>{children}</AppLayout>
+        <Toaster />
+      </body>
+    </html>
+  );
+}
--- src/shared/routes/index.ts
+++ src/shared/routes/index.ts
@@ -1,18 +1,20 @@
 import { ALBUM_PAGE_PARAMS } from "@/views/album/types";
 
+
+// TODO support a locales!
 export const ROUTES = {
-  home: "/",
-  app: "/app",
-  albums: "/app/albums",
+  home: "/en",
+  app: "/en/app",
+  albums: "/en/app/albums",
   album: ({ albumId, page }: ALBUM_PAGE_PARAMS) =>
-    `/app/album/${albumId}/${page}`,
+    `/en/app/album/${albumId}/${page}`,
   simpleReader: (sentence: string) =>
-    "/app/simple-reader?sentence=" + encodeURIComponent(sentence),
-  simpleReaderRoot: "/app/simple-reader",
-  dict: "/app/dict",
-  ocrCapture: "/app/ocr-capture",
-  settings: "/app/settings",
-  ankiImport: "/app/anki-import",
-  favorites: "/app/favorites",
-  translator: "/app/translator",
+    "/en/app/simple-reader?sentence=" + encodeURIComponent(sentence),
+  simpleReaderRoot: "/en/app/simple-reader",
+  dict: "/en/app/dict",
+  ocrCapture: "/en/app/ocr-capture",
+  settings: "/en/app/settings",
+  ankiImport: "/en/app/anki-import",
+  favorites: "/en/app/favorites",
+  translator: "/en/app/translator",
 };
